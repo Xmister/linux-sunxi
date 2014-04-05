42ceb1f1096e7b7051c0034fd7dadc0e21dff1a2
Xmister <xmisterhu@gmail.com>
2014-04-05 10:43:31 +0200

    Don't check address code
    Signed-off-by: Xmister <xmisterhu@gmail.com>

--- drivers/input/keyboard/ir-keymap.h
+++ drivers/input/keyboard/ir-keymap.h
@@ -9,7 +9,7 @@
 #define IR_IO_MAPPING			1
 
 /*IR Key Match Config*/
-#define IR_CHECK_ADDR_CODE  
+//#define IR_CHECK_ADDR_CODE  
 #define IR_ADDR_CODE		    (0x9f00)
 //#define INPUT_VALUE_MAPPING
 
